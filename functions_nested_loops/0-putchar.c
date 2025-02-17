ad164623a513416bbd27d8982d8b917100f8bc27
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-17 11:18:10 +0100

    0 task

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar followed by a new line.
+ *
+ * Return: 0 to indicate successful execution.
+ */
+int main(void)
+{
+printf("_putchar\n");  / Print _putchar followed by a new line
+return (0);  / Return 0 to indicate successful execution with parentheses
+}
