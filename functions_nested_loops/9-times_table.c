b7295350403116f6e19c4b6d82a2bb1d8272965a
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-18 17:14:21 +0100

    9 task

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,18 +1,38 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * times_table - Prints the 9 times table, starting from 0.
+ *
+ * Description: This function prints the 9 times table, one row at a time.
+ *              Each row contains the products of a number from 0 to 9,
+ *              formatted with commas and spaces as required. It uses the
+ *              putchar function to print each character.
+ */
 void times_table(void)
 {
 int i, j, product;
+
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 product = i * j;
-if (j == 0)
-printf("%d", product);
-else
-printf(", %2d", product);
+
+if (j != 0)
+{
+putchar(',');
+putchar(' ');
 }
-printf("\n");
+
+if (product < 10)
+putchar('0' + product);
+}
+else
+{
+putchar('0' + (product / 10));
+putchar('0' + (product % 10));
+}
+}
+putchar('\n');
 }
 }
