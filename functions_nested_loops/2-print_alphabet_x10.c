42ff0acb29fe485a85dda58888b7a5f66376a2aa
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-18 15:37:37 +0100

    2 task

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
- */ 
-void print_alphabet_x10(void);
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase,
+ * followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
 {
+int i;
 char letter;
 
+for (i = 0; i < 10; i++)
+{
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-_putchar(letter); /* Prints each letter */
+_putchar(letter);
 }
-
-_putchar('\n'); /* Prints a new line */
+_putchar('\n');
+}
 }
