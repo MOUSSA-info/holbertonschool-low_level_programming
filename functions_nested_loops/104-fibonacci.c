7170795b2c3161b4585548ab5abab7f8ffb96c25
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-21 12:42:27 +0100

    14 task

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Prints the first 98 Fibonacci numbers.
+* Return: Always 0.
+*/
+int main(void)
+{
+long a = 1, b = 2, next;
+int i;
+
+printf("%ld, %ld", a, b);
+
+for (i = 3; i <= 98; i++)
+{
+next = a + b;
+printf(", %ld", next);
+a = b;
+b = next;
+}
+
+printf("\n");
+return (0);
+}
+
