58fc3109879f5fbd3406721feca1ee7f4bb1bf75
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-25 12:34:18 +0100

    4 task

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
 * print_most_numbers - Prints the numbers from 0 to 9, excluding 2 and 4.
 *
 * Description: This function loops through the numbers 0 to 9 and prints each
-* number, except for 2 and 4. The numbers are printed on the same line, 
+* number, except for 2 and 4. The numbers are printed on the same line,
 * followed by a new line at the end.
 */
 void print_most_numbers(void)
