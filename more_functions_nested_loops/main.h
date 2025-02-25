598955abbfbffc82a7a674905c95067882fb527d
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-25 15:33:40 +0100

    8 task

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,10 +3,12 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_diagonal(int n);
+void print_square(int size);
 #ifndef MAIN_H
 #define MAIN_H
 
 void _putchar(char c);
+void print_square(int size);
 
 #endif /* MAIN_H */
 
