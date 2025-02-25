44c35db6bbc58b105edd751c4d192d13780cc5db
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-25 13:26:13 +0100

    6 task

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -11,3 +11,10 @@ void print_numbers(void);
 #endif /* MAIN_H */
 void print_most_numbers(void);
 void more_numbers(void);
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char c);
+void print_line(int n);
+
+#endif
