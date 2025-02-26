8dc066e44d8850f3c2d5840dbf9112f1b5820e7b
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-26 12:46:48 +0100

    1 task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int *n);
+/* Function prototype for swap_int */
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
-
