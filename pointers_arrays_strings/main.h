5ffde44c71182429570b8fbe8d649837366184b9
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-26 15:17:01 +0100

    5 task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts(char *str);
-int _putchar(char c);
+/* Function prototypes */
+void rev_string(char *s);
 
 #endif /* MAIN_H */
