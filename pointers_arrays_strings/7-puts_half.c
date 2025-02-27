031a20a7c42069de3348801d2e17b65c85a6cf42
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-27 10:32:39 +0100

    7 task

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,20 +1,19 @@
 #include "main.h"
-#include <string.h>
 
 /**
-* puts_half - Prints the second half of a string.
-* @str: The string to print half of.
-* 
-* If the string length is odd, it will print the last (n + 1) / 2 characters.
+* puts_half - Prints the second half of a string
+* @str: The string to be printed
 */
+
 void puts_half(char *str)
 {
-int len = 0;
-int start_index;
-while (str[len] != '\0')
-{
-len++;
-}
+int length = 0, start_index;
+
+while (str[length] != '\0')
+length++;
+
+start_index = (length % 2 == 0) ? (length / 2) : ((length + 1) / 2);
+
 while (str[start_index] != '\0')
 {
 _putchar(str[start_index]);
