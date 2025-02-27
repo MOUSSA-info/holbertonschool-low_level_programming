95b02ea21c036fa802e9cb699ae961cc5f94344f
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-27 20:45:46 +0100

    1 task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,9 +22,10 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Your declarations here */
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
