4bc5569959df0e8a50f82d4e1844139b773d3932
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-02-27 12:47:27 +0100

    16 task

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the first 98 Fibonacci numbers.
- * Description:
- * The program calculates andprints
- * prints the first 98 Fibonacci numbers starting with
- * 1 and 2, separated by a comma followed by a space.
- * The Fibonacci sequence is calculated using only integer operations and is
- * printed to the standard output.
- * Return: Always 0 (Success)
- */
-
+* main - finds and prints the first 98 Fibonacci numbers
+*
+* Description: The program calculates
+* prints the first 98 Fibonacci numbers
+* starting from 1 and 2, followed by a comma.
+*  a space, with a new line at the end.
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-unsigned long a = 1, b = 2;
-unsigned long temp;
+unsigned long fib1 = 1, fib2 = 2;
+unsigned long fib_next;
 int i;
 
-printf("%lu, %lu", a, b);
+printf("%lu, %lu", fib1, fib2);
 
 for (i = 3; i <= 98; i++)
 {
-temp = a + b;
-printf(", %lu", temp);
-a = b;
-b = temp;
+fib_next = fib1 + fib2;
+printf(", %lu", fib_next);
+fib1 = fib2;
+fib2 = fib_next;
 }
 
 printf("\n");
+
 return (0);
+
 }
