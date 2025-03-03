6e10f4f5cdbb0ba1526f0f51a3843483ca5a62f6
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-03 11:17:47 +0100

    3 task

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,33 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * _strcmp - Compares two strings
-* @s1: The first string to compare
-* @s2: The second string to compare
+* @s1: The first string
+* @s2: The second string
 *
-* Description:
-* This function compares two strings character by character.
-* It works exactly like the
-* standard library function `strcmp`.
-* The comparison is based on ASCII values of characters.
-*
-* Return:
-* - A negative value if s1 is less than s2.
-* - 0 if s1 is equal to s2.
-* - A positive value if s1 is greater than s2.
+* Return: 0 if s1 and s2 are equal,
+*         a negative value if s1 is less than s2,
+*         a positive value if s1 is greater than s2
 */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
+while (*s1 && (*s1 == *s2))
 {
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
+s1++;
+s2++;
 }
-i++;
-}
-
-return (s1[i] - s2[i]);
+return (0);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+int _strcmp(char *s1, char *s2);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _strcmp(char *s1, char *s2);
+
+#endif
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -23,14 +30,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-#ifndef MAIN_H
-#define MAIN_H
-
-int _strcmp(char *s1, char *s2);
-
-#endif /* MAIN_H */
-
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
