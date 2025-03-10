2a042d32023f7c19546e41e818b57e739d69c2f2
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-10 11:06:01 +0100

    4 task

--- recursion/main.h
+++ recursion/main.h
@@ -2,9 +2,10 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _pow_recursion(int x, int y);
 
 #endif /* MAIN_H */
