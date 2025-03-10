4a201cc2a41e370f88a4f64eb7589f68542a2ae3
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-10 13:25:35 +0100

    0 task

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - Prints the name of the source file
- *
- * Description: This program uses the __FILE__ macro
- * to print the name of the
- * source file from which it is compiled.
- * This allows the program to dynamically
- * print its own file name without hardcoding it.
- *
- * Return: Always 0 (success)
- */
-int main(void)
+* main - Prints the name of the program
+* @argc: The number of arguments passed to the program
+* @argv: An array of pointers to the arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
 {
-printf("Source file name: %s\n", __FILE__);
+(void)argc; /* Suppress unused variable warning */
+printf("%s\n", argv[0]);
 return (0);
 }
