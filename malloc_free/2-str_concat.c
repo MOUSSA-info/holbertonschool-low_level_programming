936297e2e4bfb8840ec1fa0dd17dc5dc6d73197b
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-13 12:06:31 +0100

    2 task

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,28 +13,24 @@ char *str_concat(char *s1, char *s2)
 {
 char *concat_str;
 int len1 = 0, len2 = 0, i, j;
-/* Treat NULL as an empty string */
+
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-/* Calculate the lengths of s1 and s2 */
+
 while (s1[len1] != '\0')
 len1++;
 while (s2[len2] != '\0')
 len2++;
-/* Allocate memory for the concatenated string */
 concat_str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-/* Check if malloc failed */
+
 if (concat_str == NULL)
 return (NULL);
-/* Copy s1 into concat_str */
 for (i = 0; i < len1; i++)
 concat_str[i] = s1[i];
-/* Copy s2 into concat_str, starting after s1 */
 for (j = 0; j < len2; j++)
 concat_str[i + j] = s2[j];
-/* Null-terminate the concatenated string */
 concat_str[i + j] = '\0';
 return (concat_str);
 }
