4438744397598cd5898b8ea7af2bcc46b28679d5
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-13 11:57:20 +0100

    1 task

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,11 +1,11 @@
-char *_strdup(char *stri);
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-#ifndef MAIN_H
-#define MAIN_H
-
 void free_grid(int **grid, int height);
 
 #endif /* MAIN_H */
