1aa489452aa1390feb4d01ddb244c271faaf1767
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-14 16:00:47 +0100

    1 task -update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,13 +7,14 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
-/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
