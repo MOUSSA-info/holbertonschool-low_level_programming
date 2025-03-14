95e842ae37c07d918dec2fa07cb02fe59ba2344e
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-14 13:48:57 +0100

    task1 project more malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,41 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
-* string_nconcat - Concatenates two strings
-* @s1: First string
-* @s2: Second string
-* @n: Number of bytes from s2 to concatenate
-*
-* Return: Pointer to newly allocated memory containing s1 + first n bytes of s2
-*         or NULL if memory allocation fails.
-*/
+ * string_nconcat - Concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ * @n: Number of bytes from s2 to concatenate
+ *
+ * Return: Pointer to newly allocated space in memory, or NULL if it fails
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1, len2, total_len;
-char *new_str;
-unsigned int i, j;
+	char *concat;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-if
-(s1 == NULL)
-s1 = "";
-if
-(s2 == NULL)
-s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-len1 = strlen(s1);
-len2 = strlen(s2);
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
-if
-(n >= len2)
-n = len2;
+	if (n >= len2)
+		n = len2;
 
-total_len = len1 + n;
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
 
-new_str = malloc(total_len + 1);
-if
-(new_str == NULL)
-return (NULL);
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
+	concat[i + j] = '\0';
 
-for (i = 0; i < len1; i++)
-new_str[i] = s1[i];
-
-for (j = 0; j < n; j++)
-new_str[i + j] = s2[j];
-
-new_str[i + j] = '\0';
-
-return (new_str);
+	return (concat);
 }
+
