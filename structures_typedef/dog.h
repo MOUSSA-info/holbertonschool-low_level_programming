b4aa2def8ac2437d6dad5bc18dfe3ee0e661a368
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-14 14:02:40 +0100

    0 task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,21 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdlib.h> // For size_t
-
 /**
- * struct dog - Represents a dog with a name, age, and owner.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - attributs du chien
+ * @name: Name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
-typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
 
-/* Function prototypes */
-dog_t *new_dog(char *name, float age, char *owner); // Add this line
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif
