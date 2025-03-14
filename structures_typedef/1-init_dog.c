74aa0543ddce42876a529dac631ace1af29a0c49
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-14 15:57:37 +0100

    1 task -update

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,17 +1,5 @@
 #include <stddef.h>
-
-/**
-* struct dog - Represents a dog with a name, age, and owner.
-* @name: The name of the dog.
-* @age: The age of the dog.
-* @owner: The owner of the dog.
-*/
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
+#include "dog.h"
 
 /**
 * init_dog - Initializes a variable of type struct dog.
@@ -22,10 +10,11 @@ char *owner;
 *
 * Return: void
 */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
-return;
+	return;
 
 d->name = name;
 d->age = age;
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,14 +7,13 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-typedef struct dog {
+struct dog {
     char *name;
     float age;
     char *owner;
-} dog_t;
+};
 
 /* Function prototypes */
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
