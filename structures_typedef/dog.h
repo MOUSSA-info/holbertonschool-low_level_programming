90b90fbea131c740a46484793073245d882ad1ec
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-14 12:23:18 +0100

    1 task

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog with a name, age, and owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+/* Function prototype for init_dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
