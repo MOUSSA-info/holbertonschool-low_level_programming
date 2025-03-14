03acffc5fb508c0a098fcc1d3135407bda2bf475
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-14 13:52:43 +0100

    2 task

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,34 +1,31 @@
 #include <stdlib.h>
-#include <string.h>
 
 /**
-* _calloc - Alloue de la mémoire pour un tableau et l'initialise à zéro.
-* @nmemb: Nombre d'éléments.
-* @size: Taille de chaque élément.
+* _calloc - Allocates memory for an array and initializes it to zero
+* @nmemb: Number of elements in the array
+* @size: Size of each element in bytes
 *
-* Return: Pointeur vers la mémoire allouée, ou NULL en cas d'échec.
+* Return: Pointer to the allocated memory, or NULL if it fails
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-/* Déclarer toutes les variables au début du bloc */
-unsigned int total_size;
 void *ptr;
+unsigned int total_size;
+unsigned char *byte_ptr;
+unsigned int i;
 
-/* Vérifier les entrées invalides */
 if (nmemb == 0 || size == 0)
-return (NULL);
+	return (NULL);
 
-/* Calculer la taille totale de la mémoire nécessaire */
 total_size = nmemb * size;
 
-/* Allouer la mémoire */
 ptr = malloc(total_size);
 if (ptr == NULL)
-return (NULL);
+	return (NULL);
 
-/* Initialiser la mémoire à zéro */
-memset(ptr, 0, total_size);
+byte_ptr = (unsigned char *)ptr;
+for (i = 0; i < total_size; i++)
+	byte_ptr[i] = 0;
 
-/* Retourner le pointeur */
 return (ptr);
 }
