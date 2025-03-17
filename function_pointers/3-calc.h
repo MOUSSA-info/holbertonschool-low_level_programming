f6bf92b717d151ae5790ff55abbe5cccbb4f3302
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-03-17 16:07:20 +0100

    3 task

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,20 +1,14 @@
-#ifndef CALC_H
-#define CALC_H
-
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
-* struct op - Struct to map operators to functions
-* @op: Operator symbol (e.g., "+")
-* @f: Corresponding function pointer
-*/
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
-char *op;
-int (*f)(int a, int b);
+    char *op;
+    int (*f)(int a, int b);
 } op_t;
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
