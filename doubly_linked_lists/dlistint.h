36c87d3907525ac7e6c5a72ccbb540db73717cf8
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-04-01 15:35:16 +0200

    7 task

--- doubly_linked_lists/dlistint.h
+++ doubly_linked_lists/dlistint.h
@@ -13,8 +13,10 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
-
-/* Function prototype for summing all data in a doubly linked list */
-int sum_dlistint(dlistint_t *head);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif
