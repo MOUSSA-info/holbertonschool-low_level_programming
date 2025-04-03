d623db108d8a0f52a18cf7817e65fd15f8ee8c31
‘MOUSSA-info’ <10434@holbertonstudents.com>
2025-04-03 13:26:27 +0200

    3 task

--- file_io/main.h
+++ file_io/main.h
@@ -4,10 +4,12 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_error(int exit_code, const char *message, const char *file_name);
+
 
 #endif
 
